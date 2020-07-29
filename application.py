bf23919bfcffb0a4dbc4e64f0ae11a0f0682a26b
sridhar279 <33222370+sridhar279@users.noreply.github.com>
2020-07-29 17:14:35 +0530

    Update application.py

--- application.py
+++ application.py
@@ -3,4 +3,4 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "<B>Hi This is a Flask Web app built using python Flask and depoloyed on Azure Web App</B>"
+    return "<B>Hi This is a Flask Web app built using python and depoloyed on Azure Web App</B>"
