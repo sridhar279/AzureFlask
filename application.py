3b185db6b8dc747c8ca7f8afbaf315f09b6c388c
sridhar279 <33222370+sridhar279@users.noreply.github.com>
2020-08-18 21:12:49 +0530

    Update application.py

--- application.py
+++ application.py
@@ -2,6 +2,6 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-@app.route('/home')
+@app.route('/home1')
 def hello():
     return "<B>Hi This is a Flask Web app built using python and depoloyed on Azure Web App</B>"
